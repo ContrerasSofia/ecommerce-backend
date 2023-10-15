72f019d4e0b0667d906a4a7f9cd0a3f434a5c7c7
Sofia Contreras <cgsofiaelizabeth15@gmail.com>
2023-10-15 11:54:28 -0400

    Category routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -68,7 +68,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(data);
+    res.status(200).json({ message: 'Tag deleted!' });
   } catch (err) {
     res.status(500).json(err);
   }
